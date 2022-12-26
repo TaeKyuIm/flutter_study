c800fc151350e5e66c21e65e561922d88c15a4b1
임태규 <kyu5787@naver.com>
2022-12-26 20:26:21 +0900

    basic flutter fix ios build error

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
+import "package:contacts_service/contacts_service.dart";
 
 void main() {
   runApp(
@@ -16,26 +18,60 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  var name = ['홍길동', '이순신', '장영실'];
-  var total = 3;
-  addUser(String user) {
+  List<Contact> name = [];
+  addUser(String givenName, String familyName) {
+    var newPerson = new Contact();
+    newPerson.givenName = givenName;
+    newPerson.familyName = familyName;
+
     setState(() {
-      name.add(user);
-      total++;
+      ContactsService.addContact(newPerson);
+      name.add(newPerson);
     });
+  }
+
+  getPermission() async {
+    var status = await Permission.contacts.status;
+    if (status.isGranted) {
+      print('허락됨');
+      var contacts = await ContactsService.getContacts();
+      setState(() {
+        name = contacts;
+      });
+
+      // var newPerson = new Contact();
+      // newPerson.givenName = '민수';
+      // newPerson.familyName = '김';
+
+      // ContactsService.addContact(newPerson);
+
+    } else if (status.isDenied){
+      print('거절됨');
+      Permission.contacts.request();
+      openAppSettings();
+    }
+  }
+
+  @override
+  void initState() {
+    getPermission();
+    // TODO: implement initState
+    super.initState();
   }
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text('연락처 어플')),
+        appBar: AppBar(title: Text('연락처 어플'), actions: [
+          IconButton(onPressed: (){ getPermission(); }, icon: Icon(Icons.contacts))
+        ],),
         body: ListView.builder(
-          itemCount: total,
+          itemCount: name.length,
           itemBuilder: (context, i) {
             return ListTile(
               leading: Icon(Icons.account_circle),
-              title: Text(name[i])
+              title: Text(name[i].givenName ?? '이름이 없음')
             );
           }),
         floatingActionButton: FloatingActionButton(
@@ -52,7 +88,8 @@ class _MyAppState extends State<MyApp> {
 class DialogUI extends StatelessWidget {
   DialogUI({Key? key, this.addUser }) : super(key: key);
   final addUser;
-  var inputData = TextEditingController();
+  var givenName = TextEditingController();
+  var familyName = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Dialog(
@@ -61,14 +98,30 @@ class DialogUI extends StatelessWidget {
         height: 300,
         child: Column(
           children: [
-            TextField(controller: inputData,),
-            TextButton(onPressed: (() {
-              addUser(inputData.text);
-              Navigator.pop(context);
-            }), child: Text('완료')),
-            TextButton(onPressed: () {
-              Navigator.pop(context);
-            }, child: Text('취소'))
+            Text('이름'),
+            TextField(controller: givenName,
+            decoration: InputDecoration(
+              enabledBorder: OutlineInputBorder(
+                borderSide: BorderSide(
+                  color: Colors.green,
+                  width: 1.0,
+                )
+              )
+            ),),
+            Text('성'),
+            TextField(controller: familyName,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                TextButton(onPressed: (() {
+                  addUser(givenName.text, familyName.text);
+                  Navigator.pop(context);
+                }), child: Text('완료')),
+                TextButton(onPressed: () {
+                  Navigator.pop(context);
+                }, child: Text('취소')),
+              ],
+            )
           ],
         ),
       ),
